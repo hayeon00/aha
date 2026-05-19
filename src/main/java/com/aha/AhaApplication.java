b0004bf446e7b9ffd07aa727c7c130a0336407a2
hayeon00 <rlagkdus1024@naver.com>
2026-05-19 05:58:09 -0300

    init: create Aha backend project

---
+++ src/main/java/com/aha/AhaApplication.java
@@ -0,0 +1,13 @@
+package com.aha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AhaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aha/AhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
