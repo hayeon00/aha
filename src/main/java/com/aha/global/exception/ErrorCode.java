3fc7f840cb0c4ee6d758feaa51f4eecd55c451cd
KIMGWANGHO <kkh185923@naver.com>
2026-06-05 16:38:54 +0900

    feat(ErrorCode): EXAM 및 WORKBOOK 도메인 에러코드 추가

--- src/main/java/com/aha/global/exception/ErrorCode.java
+++ src/main/java/com/aha/global/exception/ErrorCode.java
@@ -2,9 +2,9 @@ package com.aha.global.exception;
 
 public enum ErrorCode {
 
-
     INVALID_INPUT_VALUE(400, "COMMON_001", "잘못된 입력값입니다."),
     ENTITY_NOT_FOUND(404, "COMMON_002", "대상을 찾을 수 없습니다."),
+    INVALID_REQUEST_FORMAT(400, "COMMON_003", "잘못된 요청 형식입니다."),
     INTERNAL_SERVER_ERROR(500, "COMMON_999", "서버 내부 오류입니다."),
 
     UNAUTHORIZED(401, "AUTH_001", "로그인이 필요합니다."),
@@ -14,7 +14,16 @@ public enum ErrorCode {
     INVALID_TOKEN(401, "AUTH_005", "유효하지 않은 토큰입니다."),
     EXPIRED_TOKEN(401, "AUTH_006", "만료된 토큰입니다."),
 
-    USER_NOT_FOUND(404, "USER_001", "사용자를 찾을 수 없습니다.");
+    USER_NOT_FOUND(404, "USER_001", "사용자를 찾을 수 없습니다."),
+
+    EXAM_NOT_FOUND(404, "EXAM_NOT_FOUND", "해당 시험이 존재하지 않습니다."),
+    EXAM_INACTIVE(400, "EXAM_INACTIVE", "해당 시험은 비활성화 상태입니다."),
+
+    WORKBOOK_TYPE_NOT_FOUND(404, "WORKBOOK_TYPE_NOT_FOUND", "문제집 유형을 찾을 수 없습니다."),
+    WORKBOOK_NOT_FOUND(404, "WORKBOOK_NOT_FOUND", "해당 워크북이 존재하지 않습니다."),
+    WORKBOOK_TYPE_UNSUPPORTED_EXAM(422,"WORKBOOK_TYPE_UNSUPPORTED_EXAM","해당 워크북 유형은 선택하신 시험을 지원하지 않습니다."),
+    WORKBOOK_ATTEMPT_ALREADY_EXIST(422,"WORKBOOK_ATTEMPT_ALREADY_EXIST","이미 해당 워크북 풀이 중입니다."),
+    WORKBOOK_ARCHIVED(422,"WORKBOOK_ARCHIVED" ,"해당 워크북은 서버 보관 중입니다." );
 
     private final int status;
     private final String code;
