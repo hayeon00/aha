45e873f70ed381030d5a9fcd450b49afa01feb62
KIMGWANGHO <kkh185923@naver.com>
2026-06-05 16:37:57 +0900

    feat(BusinessException): data 필드 추가 및 필드 레벨 @Getter 추가 및 생성자 추가

--- src/main/java/com/aha/global/exception/BusinessException.java
+++ src/main/java/com/aha/global/exception/BusinessException.java
@@ -1,20 +1,29 @@
 package com.aha.global.exception;
 
+import lombok.Getter;
+
 public class BusinessException extends RuntimeException {
 
+    @Getter
     private final ErrorCode errorCode;
+    private final Object data;
 
     public BusinessException(ErrorCode errorCode) {
         super(errorCode.getMessage());
         this.errorCode = errorCode;
+        this.data=null;
     }
 
-    public BusinessException(ErrorCode errorCode, String message) {
+    public BusinessException(ErrorCode errorCode, String message, Object data) {
         super(message);
         this.errorCode = errorCode;
+        this.data = data;
     }
 
-    public ErrorCode getErrorCode() {
-        return errorCode;
-    }
+  public BusinessException(ErrorCode errorCode, Object data) {
+    super(errorCode.getMessage());
+    this.errorCode = errorCode;
+    this.data = data;
+  }
+
 }
