6727a7ceb91e5fb146c57ef57fb2b7ac4769060f
KIMGWANGHO <kkh185923@naver.com>
2026-06-05 16:53:03 +0900

    feat(ExamVersionRepository): repository 구현

---
+++ src/main/java/com/aha/domain/exam/repository/ExamVersionRepository.java
@@ -0,0 +1,9 @@
+package com.aha.domain.exam.repository;
+
+import com.aha.domain.exam.entity.ExamVersion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExamVersionRepository extends JpaRepository<ExamVersion, Long> {
+
+
+}
