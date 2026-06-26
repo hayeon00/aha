742c186c8b31888725a232d3b4d05a42ce75c79a
hayeon00 <rlagkdus1024@naver.com>
2026-06-26 11:48:31 +0900

    refactor: 문서 처리 도메인 기존 구조 정리(클래스 제거)

--- src/main/java/com/aha/domain/ailearn/document/dto/mapping/request/ExamScopeCandidateDto.java
+++
@@ -1,8 +0,0 @@
-package com.aha.domain.ailearn.document.dto.mapping.request;
-
-public record ExamScopeCandidateDto(
-        Long examScopeNodeId,
-        String title,
-        String path
-) {
-}
--- src/main/java/com/aha/domain/ailearn/document/dto/mapping/response/DocumentScopeMappingResultDto.java
+++
@@ -1,9 +0,0 @@
-package com.aha.domain.ailearn.document.dto.mapping.response;
-
-import java.math.BigDecimal;
-
-public record DocumentScopeMappingResultDto(
-        Long examScopeNodeId,
-        String mappingReason
-) {
-}
