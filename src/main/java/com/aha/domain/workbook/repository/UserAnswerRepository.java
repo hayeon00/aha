9685784bc15b30c757a62caeb4f5ab4c4ab01d0d
KIMGWANGHO <kkh185923@naver.com>
2026-07-20 13:29:55 +0900

    feat(UserAnswerRepository): 사용자 답안 개수 조회하는 메서드 추가 , attemptId로 UserAnswer을 Problem과 ExamScopeNode로 조인해오는 메서드 추가

--- src/main/java/com/aha/domain/workbook/repository/UserAnswerRepository.java
+++ src/main/java/com/aha/domain/workbook/repository/UserAnswerRepository.java
@@ -4,10 +4,22 @@ import com.aha.domain.workbook.entity.UserAnswer;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserAnswerRepository extends JpaRepository<UserAnswer,Long> {
 
     List<UserAnswer> findByWorkbookAttempt_Id(Long attemptId);
 
     Optional<UserAnswer> findByProblem_IdAndWorkbookAttempt_Id(Long problemId, Long attemptId);
+
+    int countByWorkbookAttempt_Id(Long attemptId);
+
+    @Query("""
+    select ua from UserAnswer ua
+    join fetch ua.problem p
+    join fetch p.examScopeNode esn
+    where ua.workbookAttempt.id = :attemptId
+""")
+    List<UserAnswer> findByAttempt_IdWithProblemAndExamScopeNode(@Param("attemptId")Long attemptId);
 }
