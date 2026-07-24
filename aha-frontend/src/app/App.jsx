5176ac52ffbf606861371d6c3bbf4f7d06411ee3
KIMGWANGHO <kkh185923@naver.com>
2026-07-24 18:24:50 +0900

    refactor(App): 복원 기출 페이지 라우트 연결

--- aha-frontend/src/app/App.jsx
+++ aha-frontend/src/app/App.jsx
@@ -5,9 +5,10 @@ import SignupPage from "../features/auth/pages/SignupPage.jsx";
 import AiLearningPage from "../features/ailearn/pages/AiLearningPage.jsx";
 import MainPage from "../features/home/pages/MainPage.jsx";
 import MyPage from "../features/user/pages/MyPage.jsx";
-import WorkbookPage from "../features/workbook/pages/WorkbookPage.jsx";
-import WorkbookAttemptPage from "../features/workbook/pages/WorkbookAttemptPage.jsx";
-import WorkbookResultPage from "../features/workbook/pages/WorkbookResultPage.jsx";
+import PastPaperListPage from "../features/pastpaper/pages/PastPaperListPage.jsx";
+import PastPaperAttemptPage from "../features/pastpaper/pages/PastPaperAttemptPage.jsx";
+import PastPaperResultPage from "../features/pastpaper/pages/PastPaperResultPage.jsx";
+import PastPaperExplanationPage from "../features/pastpaper/pages/PastPaperExplanationPage.jsx";
 import MainLayout from "../common/layouts/MainLayout.jsx";
 import { logout } from "../features/auth/api/authApi.js";
 
@@ -132,16 +133,20 @@ function App() {
                     element={<AiLearningPage />}
                 />
                 <Route
-                    path="/problems"
-                    element={<WorkbookPage />}
+                    path="/past-papers"
+                    element={<PastPaperListPage />}
                 />
                 <Route
-                    path="/problems/:workbookId/attempts/:attemptId"
-                    element={<WorkbookAttemptPage />}
+                    path="/past-papers/:pastPaperId/attempts/:attemptId"
+                    element={<PastPaperAttemptPage />}
                 />
                 <Route
-                    path="/problems/:workbookId/attempts/:attemptId/result"
-                    element={<WorkbookResultPage />}
+                    path="/past-papers/:pastPaperId/attempts/:attemptId/result"
+                    element={<PastPaperResultPage />}
+                />
+                <Route
+                    path="/past-papers/:pastPaperId/attempts/:attemptId/explanation"
+                    element={<PastPaperExplanationPage />}
                 />
                 <Route
                     path="/mypage"
