9a8dde8ee53fecf6a0589e8fb3f45b2dcbbb8d5e
KIMGWANGHO <kkh185923@naver.com>
2026-07-24 18:20:20 +0900

    refactor(PastPaperController): 복원 기출 목록 조회 API 추가

---
+++ src/main/java/com/aha/domain/pastpaper/controller/PastPaperController.java
@@ -0,0 +1,30 @@
+package com.aha.domain.pastpaper.controller;
+
+import com.aha.domain.pastpaper.dto.response.PastPaperResponseDto;
+import com.aha.domain.pastpaper.service.PastPaperService;
+import com.aha.global.response.ApiResponse;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class PastPaperController {
+
+    private final PastPaperService pastPaperService;
+
+    @GetMapping("/exam-versions/{versionId}/past-papers")
+    public ResponseEntity<ApiResponse<List<PastPaperResponseDto>>> getPastPapers(
+        @PathVariable("versionId") Long versionId
+    ){
+
+        return ResponseEntity.ok()
+            .body(ApiResponse.success(200,"복원 기출 목록 조회 성공",pastPaperService.getPastPapers(versionId)));
+    }
+
+}
