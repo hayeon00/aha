c46be4ffac8b35e9a909affd8ae06bfbe010475b
KIMGWANGHO <kkh185923@naver.com>
2026-08-03 14:36:39 +0900

    feat(StudyRoomMemberRepository): 스터디룸 멤버 저장소 추가

---
+++ src/main/java/com/aha/domain/study/repository/StudyRoomMemberRepository.java
@@ -0,0 +1,8 @@
+package com.aha.domain.study.repository;
+
+import com.aha.domain.study.entity.StudyRoomMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudyRoomMemberRepository extends JpaRepository<StudyRoomMember,Long> {
+
+}
