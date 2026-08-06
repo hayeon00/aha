b5362b5dad530e7cb732eb70b0fef448234f33fa
KIMGWANGHO <kkh185923@naver.com>
2026-08-06 16:08:06 +0900

    feat(ActiveStudyRoomParticipationRepository): 활성 스터디룸 조회 쿼리 추가

--- src/main/java/com/aha/domain/study/repository/ActiveStudyRoomParticipationRepository.java
+++ src/main/java/com/aha/domain/study/repository/ActiveStudyRoomParticipationRepository.java
@@ -3,12 +3,19 @@ package com.aha.domain.study.repository;
 import com.aha.domain.study.entity.ActiveStudyRoomParticipation;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ActiveStudyRoomParticipationRepository extends JpaRepository<ActiveStudyRoomParticipation,Long> {
 
     boolean existsByUserId(Long userId);
 
-    Optional<ActiveStudyRoomParticipation> findByUserId(Long userId);
+    @Query("""
+    select asrp from ActiveStudyRoomParticipation asrp
+      join fetch asrp.studyRoom
+      where asrp.userId = :userId
+""")
+    Optional<ActiveStudyRoomParticipation> findByUserIdWithStudyRoom(@Param("userId") Long userId);
 
     Optional<ActiveStudyRoomParticipation> findByStudyRoom_IdAndUserId(Long studyRoomId, Long userId);
 }
